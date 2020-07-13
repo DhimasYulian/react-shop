f290fab8e0c2ac267bb01110e75ba8f8abdf6bfa
DhimasYulian <kekto7657@gmail.com>
2020-07-13 10:58:59 +0700

    redux for product

---
+++ src/reducer/productReducer.js
@@ -0,0 +1,10 @@
+export const productReducer = (state = {}, action) => {
+    switch (action.type) {
+        case "FETCH_PRODUCTS":
+            return {
+                items: action.payload
+            }
+            default:
+                return state;
+    }
+}
